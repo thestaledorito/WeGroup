e034e35e653312573ac414a73e68d88c279de398
chrisburnham <cburnham15@gmail.com>
2017-11-06 19:57:32 -0500

    adding more data details

--- src/tcp_bridge/Tcp_server_side.java
+++ src/tcp_bridge/Tcp_server_side.java
@@ -1,10 +1,38 @@
 package tcp_bridge;
 
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
 //This will do any Client Specific actions we decide 
 //we need for the bridge
-public class Tcp_server_side extends Tcp_bridge {
+public class Tcp_server_side {
+		
+	public void Init()
+	{
+		try
+		{
+			m_server = new ServerSocket(6969);
+		}
+		catch(IOException e)
+		{
+			System.out.println("Error creating server:" + e);
+		}
+	}
 	
-
+	// Called in a loop to see if server has a new client
+	private void check_for_incoming_connection()
+	{
+		
+	}
+	
+	private void check_clients_alive()
+	{
+		
+	}
+	
+	private ServerSocket m_server;
+	
+	private List<Tcp_bridge> m_client_connections;
 
 }
