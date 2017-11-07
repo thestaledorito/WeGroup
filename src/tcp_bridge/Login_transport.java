48025b6f1ee90b7fc4c38a07fb504537bfdf0e15
chrisburnham <cburnham15@gmail.com>
2017-11-06 20:15:19 -0500

    adding reply to login message type with port num

--- src/tcp_bridge/Login_transport.java
+++ src/tcp_bridge/Login_transport.java
@@ -1,5 +1,8 @@
 package tcp_bridge;
 
 public class Login_transport extends Transport_base {
-
+	String m_user_name;
+	
+	// Do we need to bother hashing?
+	String m_password;
 }
---
+++ src/tcp_bridge/Negotiate_transport.java
@@ -0,0 +1,5 @@
+package tcp_bridge;
+
+public class Negotiate_transport extends Transport_base {
+	int m_port_number;
+}
