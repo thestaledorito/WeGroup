6c5207d35f1f817c79e04fbba50d7a9ae72c8047
chrisburnham <cburnham15@gmail.com>
2017-11-12 08:12:48 -0500

    adding tcp init to both sides

--- src/client/Main_page.java
+++ src/client/Main_page.java
@@ -1,5 +1,24 @@
 package client;
 
+import data_types.*;
+import tcp_bridge.Tcp_client_side;
+
 public class Main_page {
 
+	// Initialize this class
+	public void Init()
+	{
+		m_tcp = new Tcp_client_side();
+		m_tcp.Init();
+		m_tcp.Register_reciver(this);
+	}
+	
+	// Data received from TCP
+	public void Data_received(Base_data data)
+	{
+		
+	}
+	
+	// Class to send TCP
+	private Tcp_client_side m_tcp;
 }
--- src/server/Database_manager.java
+++ src/server/Database_manager.java
@@ -1,5 +1,24 @@
 package server;
 
-public class Database_manager {
+import tcp_bridge.Tcp_server_side;
+import data_types.*;
 
+public class Database_manager {
+	
+	// Initialize this class
+	public void Init()
+	{
+		m_tcp = new Tcp_server_side();
+		m_tcp.Init();
+		m_tcp.Register_reciver(this);
+	}
+	
+	// Data received from TCP
+	public void Data_received(Base_data data)
+	{
+		
+	}
+
+	// Class to send TCP
+	private Tcp_server_side m_tcp;
 }
