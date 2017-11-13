e52411296a3d2f8bc4b9536d342a89e1b70287b2
chrisburnham <cburnham15@gmail.com>
2017-11-13 06:58:28 -0500

    adding test code

---
+++ src/testing/Client_side.java
@@ -0,0 +1,14 @@
+package testing;
+
+import client.Main_page;
+
+public class Client_side extends Main_page 
+{
+
+	public static void main(String[] args) 
+	{
+	
+
+	}
+
+}
---
+++ src/testing/Server_side.java
@@ -0,0 +1,13 @@
+package testing;
+
+import server.Database_manager;
+
+public class Server_side extends Database_manager 
+{
+
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
