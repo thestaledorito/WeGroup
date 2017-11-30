d8248ce6c30cfae1b55df56594d493ed378080a9
RikerBillings <rikerbillz@gmail.com>
2017-11-29 21:53:55 -0500

    Started Message_server
    Beginning of Message server. Thinking of not using a private message class. just a boolean flag that will set it to private or not

--- src/server/Message_server.java
+++ src/server/Message_server.java
@@ -1,6 +1,31 @@
 package server;
 
+import java.util.List;
+
 public class Message_server 
 {
 
+	//String msg_id;
+	String message;
+	List<String> recipients;
+	boolean pvt;
+	
+	/**
+	 * 
+	 * @param msg 	the message to be sent
+	 * @param target	the recipients for the message. if 1 user, send in a list still
+	 * @param pvt	privateMessage flag. If True, the message is intended for that 1 user.
+	 */
+	public Message_server(String msg, List<String> target, Boolean pvt) {
+		this.message = msg;
+	}
+	
+	/**
+	 * Checks if the message is private or not
+	 * @return	True if the message is private
+	 */
+	public boolean isPvt() {
+		return pvt;
+	}
+		
 }
