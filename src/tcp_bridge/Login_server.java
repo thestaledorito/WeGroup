7b035529950d5b4e31e6c23bc67b84af10e3643c
chrisburnham <cburnham15@gmail.com>
2017-12-03 10:27:03 -0500

    starting up login server

--- src/tcp_bridge/Login_server.java
+++ src/tcp_bridge/Login_server.java
@@ -1,5 +1,42 @@
 package tcp_bridge;
 
-public class Login_server extends Tcp_bridge_server {
+import java.util.*;
 
+public class Login_server extends Tcp_bridge_server 
+{
+	public Login_server()
+	{
+		m_users = new ArrayList<User_login>();
+	}
+	
+	public void Add_user(String user_name, String group_name, String password)
+	{
+		if(!Contains_user(user_name, group_name))
+		{
+			User_login new_user = new User_login();
+			new_user.m_user_name = user_name;
+			new_user.m_group_name = group_name;
+			new_user.m_password = password;
+			
+			m_users.add(new_user);
+		}
+	}
+	
+	public boolean Contains_user(String user_name, String group_name)
+	{
+		// Iterated through m_users
+		// If we have a match of user and group name true
+		// Otherwise false
+		
+		return false;
+	}
+	
+	public boolean Valid_login(String user_name, String group_name, String password)
+	{
+		return true;
+	}
+	
+	// private iterator function for above 2?
+	
+	private List<User_login> m_users;
 }
---
+++ src/tcp_bridge/User_login.java
@@ -0,0 +1,17 @@
+package tcp_bridge;
+
+public class User_login 
+{
+	public User_login()
+	{
+		m_user_name = "";
+		m_group_name = "";
+		m_password = "";
+	}
+	
+	public String m_user_name;
+	
+	public String m_group_name;
+	
+	public String m_password;
+}
